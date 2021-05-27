308a32fe91bffb140547f6cda6dc931f976f84c1
Bruno Gomes <icestonebruno@gmail.com>
2021-05-27 17:11:52 -0300

    array de episodios

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const fileAPI = require('./db.json')
 
-console.log(fileAPI)
+/* console.log(fileAPI) */
+console.log(fileAPI.episodes)
+
 console.log(process.env.PORT)
 
 app.use('/', (req,res) => {
   //res.send('index.html')
-  return res.json(fileAPI)
+  return res.json(fileAPI.episodes)
 })
 
 app.listen(process.env.PORT || 3333, () => console.log('SERVER IS RUNNING'))
